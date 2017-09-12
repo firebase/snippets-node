27ff1c727b4095da2d0362c5467bc87608af60c9
Abe Haskins <abeisgreat@abeisgreat.com>
2017-09-12 10:55:49 -0700

    Adds better algolia snippets

--- firestore/full-text-search/functions/index.js
+++ firestore/full-text-search/functions/index.js
@@ -1,8 +1,8 @@
-const functions = require('firebase-functions');
-const firestore = require('firebase-functions/lib/providers/datastore');
-const Firestore = require('@google-cloud/firestore');
+const functions = require("firebase-functions");
+const firestore = require("firebase-functions/lib/providers/datastore");
+const Firestore = require("@google-cloud/firestore");
 
-const algoliasearch = require('algoliasearch');
+const algoliasearch = require("algoliasearch");
 
 // [START init_algolia]
 // Initialize Algolia, requires installing Algolia dependencies:
@@ -12,60 +12,88 @@ const algoliasearch = require('algoliasearch');
 const ALGOLIA_ID = functions.config().algolia.app_id;
 const ALGOLIA_ADMIN_KEY = functions.config().algolia.api_key;
 
-const ALGOLIA_INDEX_NAME = 'blogposts';
+const ALGOLIA_INDEX_NAME = "notes";
 const client = algoliasearch(ALGOLIA_ID, ALGOLIA_ADMIN_KEY);
 // [END init_algolia]
 
 // Initialize Firestore
 const firestoreOptions = {
-    projectId: process.env.GCLOUD_PROJECT
+  projectId: process.env.GCLOUD_PROJECT
 };
 const db = new Firestore(firestoreOptions);
 
 // [START update_index_function]
 // Update the search index every time a blog post is written.
-exports.indexBlogPost = firestore.document('posts/{postId}').onWrite(event => {
-    // Get the blog post document
-    const text = event.data.data().text;
+exports.onNoteCreated = firestore.document("notes/{noteId}").onCreate(event => {
+  // Get the note document
+  const note = event.data.data();
 
-    // Construct a JSON representation
-    const postObject = {
-        text: text,
-        objectID: event.params.postId
-    };
+  // Add an "objectID" field which Algolia requires
+  note.objectID = event.params.postId;
 
-    // Write to the algolia index
-    const index = client.initIndex(ALGOLIA_INDEX_NAME);
-    return index.saveObject(postObject);
+  // Write to the algolia index
+  const index = client.initIndex(ALGOLIA_INDEX_NAME);
+  return index.saveObject(note);
 });
 // [END update_index_function]
 
-// [START search_request_function]
-// When a client writes a new request to 'post_search_requests', search the blog post
-// index and write the response back to `post_search_responses'
-exports.searchRequest = firestore.document('post_search_requests/{searchId}').onWrite(event => {
-    // Get the search ID and query from the event
-    const searchId = event.params.searchId;
-    const query = event.data.data().query;
+// [START get_algolia_user_token]
+const express = require("express");
+const admin = require("admin");
+const cors = require("cors");
 
-    // Query the algolia index
-    const index = client.initIndex(ALGOLIA_INDEX_NAME);
-    return index.search(query).then(content => {
-        // Search success, write response back to post_search_responses.
-        // The 'content' variable is an algolia search response
-        // https://www.algolia.com/doc/api-client/javascript/search/#search-response-format
-        return db.collection('post_search_responses')
-            .doc(searchId)
-            .set({
-                content: content
-            });
-    }).catch(err => {
-        // Search failed, write error to post_search_responses.
-        return db.collection('post_search_responses')
-            .doc(searchId)
-            .set({
-                error: err
-            });
+function getFirebaseUser(req, res, next) {
+  console.log("Check if request is authorized with Firebase ID token");
+
+  if (
+    !req.headers.authorization ||
+    !req.headers.authorization.startsWith("Bearer ")
+  ) {
+    console.error(
+      "No Firebase ID token was passed as a Bearer token in the Authorization header.",
+      "Make sure you authorize your request by providing the following HTTP header:",
+      "Authorization: Bearer <Firebase ID Token>"
+    );
+    res.status(403).send("Unauthorized");
+    return;
+  }
+
+  let idToken;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer ")
+  ) {
+    console.log("Found 'Authorization' header");
+    // Read the ID Token from the Authorization header.
+    idToken = req.headers.authorization.split("Bearer ")[1];
+  }
+
+  admin
+    .auth()
+    .verifyIdToken(idToken)
+    .then(decodedIdToken => {
+      console.log("ID Token correctly decoded", decodedIdToken);
+      req.user = decodedIdToken;
+      next();
+    })
+    .catch(error => {
+      console.error("Error while verifying Firebase ID token:", error);
+      res.status(403).send("Unauthorized");
     });
+}
+
+const app = express();
+app.use(cors({ origin: true }));
+app.use(getFirebaseUser);
+app.get("/", (req, res) => {
+  const params = {
+    hitsPerPage: 20,
+    filters: `author:${req.user.user_id}`,
+    userToken: req.user.user_id
+  };
+  const key = search.generateSecuredApiKey(ALGOLIA_SEARCH_KEY, params);
+  res.json({ key });
 });
-// [END search_request_function]
+
+exports.getSearchKey = functions.https.onRequest(app);
+// [END get_algolia_user_token]
---
+++ firestore/full-text-search/public/index.js
@@ -0,0 +1,35 @@
+// [START search_index_unsecure]
+const client = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_SEARCH_KEY);
+const index = client.initIndex("notes");
+
+index
+  .query({
+    query: "Some text"
+  })
+  .then(responses => {
+    console.log(responses.hits);
+  });
+// [END search_index_unsecure]
+
+// [START search_index_secure]
+const projectID = "calendar-cafe";
+
+fetch(`https://us-central1-${projectID}.cloudfunctions.net/getSearchKey/`, {
+  headers: { Authorization: `Bearer ${token}` }
+})
+  .then(r => {
+    return r.json();
+  })
+  .then(data => {
+    this.algolia.client = algoliasearch(ALGOLIA_APP_ID, data.key);
+    this.algolia.index = this.algolia.client.initIndex("notes");
+
+    index
+      .query({
+        query: "Some text"
+      })
+      .then(responses => {
+        console.log(responses.hits);
+      });
+  });
+// [END search_index_secure]
