867dba2fb4c825e943360a44bb5e63b2782492bf
Abe Haskins <abeisgreat@abeisgreat.com>
2017-09-19 12:47:45 -0700

    Adds fs_onsnapshot

--- firestore/presence/public/index.js
+++ firestore/presence/public/index.js
@@ -1,3 +1,4 @@
+function rtdb_presence() {
 // [START rtdb_presence]
 // Fetch the current user's ID from Firebase Authentication.
 const uid = firebase.auth().currentUser.uid;
@@ -9,14 +10,14 @@ const userStatusDatabaseRef = firebase.database().ref(`/status/${uid}`);
 // We'll create two constants which we will write to 
 // the Realtime database when this device is offline
 // or online.
-const isOffline = {
+const isOfflineForDatabase = {
     state: "offline",
-    last_seen: firebase.database.ServerValue.TIMESTAMP,
+    last_changed: firebase.database.ServerValue.TIMESTAMP,
 };
 
-const isOnline = {
-    state: "offline",
-    last_seen: firebase.database.ServerValue.TIMESTAMP,
+const isOnlineForDatabase = {
+    state: "online",
+    last_changed: firebase.database.ServerValue.TIMESTAMP,
 };
 
 // Create a reference to the special ".info/connected" path in 
@@ -32,7 +33,7 @@ firebase.database().ref(".info/connected").on("value", function (snapshot) {
     // method to add a set which will only trigger once this 
     // client has disconnected by closing the app, 
     // losing internet, or any other means.
-    userStatusDatabaseRef.onDisconnect().set(isOffline).then(function () {
+    userStatusDatabaseRef.onDisconnect().set(isOfflineForDatabase).then(function () {
         // The promise returned from .onDisconnect().set() will
         // resolve as soon as the server acknowledges the onDisconnect() 
         // request, NOT once we've actually disconnected:
@@ -40,24 +41,67 @@ firebase.database().ref(".info/connected").on("value", function (snapshot) {
 
         // We can now safely set ourselves as "online" knowing that the
         // server will mark us as offline once we lose connection.
-        userStatusDatabaseRef.set(isOnline);
+        userStatusDatabaseRef.set(isOnlineForDatabase);
     });
 });
 // [END rtdb_presence]
+}
 
+function rtdb_and_local_fs_presence() {
 // [START rtdb_and_local_fs_presence]
-[START_EXCLUDE]
-[END_EXCLUDE]
+// [START_EXCLUDE]
+const uid = firebase.auth().currentUser.uid;
 const userStatusDatabaseRef = firebase.database().ref(`/status/${uid}`);
+
+const isOfflineForDatabase = {
+    state: "offline",
+    last_changed: firebase.database.ServerValue.TIMESTAMP,
+};
+
+const isOnlineForDatabase = {
+    state: "online",
+    last_changed: firebase.database.ServerValue.TIMESTAMP,
+};
+
+// [END_EXCLUDE]
 const userStatusFirestoreRef = firebase.firestore().doc(`/status/${uid}`);
+
+// Firestore uses a different server timestamp value, so we'll 
+// create two more constants for Firestore state.
+const isOfflineForFirestore = {
+    state: "offline",
+    last_changed: firebase.firestore.FieldValue.serverTimestamp(),
+};
+
+const isOnlineForFirestore = {
+    state: "online",
+    last_changed: firebase.firestore.FieldValue.serverTimestamp(),
+};
 
 firebase.database().ref(".info/connected").on("value", function (snapshot) {
     if (snapshot.val() == false) {
+        // Instead of simply returning, we'll also set Firestore's state
+        // to "offline". This ensures that our Firestore cache is aware
+        // of the switch to "offline."
+        userStatusFirestoreRef.set(isOfflineForFirestore);
         return;
     };
 
-    userStatusRef.onDisconnect().set(isOffline).then(function () {
-        userStatusRef.set(isOnline);
+    userStatusDatabaseRef.onDisconnect().set(isOfflineForDatabase).then(function () {
+        userStatusDatabaseRef.set(isOnlineForDatabase);
+
+        // We'll also add Firestore set here for when we come online.
+        userStatusFirestoreRef.set(isOnlineForFirestore);
     });
 });
 // [END rtdb_and_local_fs_presence]
+}
+
+function fs_listen() {
+// [START fs_onsnapshot]
+userStatusFirestoreRef.onSnapshot(function (doc) {
+    const isOnline = doc.data().state == "online";
+    // ... use isOnline
+});
+// [END fs_onsnapshot]
+}
