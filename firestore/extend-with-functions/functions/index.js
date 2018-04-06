a2a7b6763c5bfd3b7eb833742086cb3b74a71375
Nicolas Garnier <nivco.las@gmail.com>
2018-04-06 21:39:44 +0200

    Fixed indent

--- firestore/extend-with-functions/functions/index.js
+++ firestore/extend-with-functions/functions/index.js
@@ -66,14 +66,14 @@ function triggerDocumentAnyChange() {
   exports.modifyUser = functions.firestore
       .document('users/{userID}')
       .onWrite((change, context) => {
-          // Get an object with the current document value.
-          // If the document does not exist, it has been deleted.
-          const document = change.after.exists ? change.after.data() : null;
+        // Get an object with the current document value.
+        // If the document does not exist, it has been deleted.
+        const document = change.after.exists ? change.after.data() : null;
 
-          // Get an object with the previous document value (for update or delete)
-          const oldDocument = change.before.data();
+        // Get an object with the previous document value (for update or delete)
+        const oldDocument = change.before.data();
 
-          // perform desired operations ...
+        // perform desired operations ...
       });
   // [END trigger_document_any_change]
 }
