39d225bb3d16c0e7edd1283c308c110714ba7559
egilmorez <egilmore@google.com>
2018-05-21 15:37:50 -0700

    Update snippet for preventing infinite loops
    Per internal bug 74829410.

--- firestore/extend-with-functions/functions/index.js
+++ firestore/extend-with-functions/functions/index.js
@@ -115,7 +115,7 @@ function writingData() {
 
         // We'll only update if the name has changed.
         // This is crucial to prevent infinite loops.
-        if (data.name == previousData.name) return;
+        if (data.name == previousData.name) return null;
 
         // Retrieve the current count of name changes
         let count = data.name_change_count;
