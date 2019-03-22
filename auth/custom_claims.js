26759b69c8160ba21395fb7a2dbda263f53ce4d2
Jen Person <personjeh@gmail.com>
2019-03-22 09:45:37 -0700

    Adding snippets for additional Admin Auth guide pages
    Adding snippets for custom-claims, email-action-links, and import-users
    Prior to now, snippets have been hard-coded in the guides.

---
+++ auth/custom_claims.js
@@ -0,0 +1,22 @@
+'use strict';
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+// [START set_custom_user_claims]
+// Set admin privilege on the user corresponding to uid.
+
+admin.auth().setCustomUserClaims(uid, {admin: true}).then(() => {
+  // The new custom claims will propagate to the user's ID token the
+  // next time a new one is issued.
+});
+// [END set_custom_user_claims]
+
+
+// [START verify_custom_claims]
+// Verify the ID token first.
+admin.auth().verifyIdToken(idToken).then((claims) => {
+  if (claims.admin === true) {
+    // Allow access to requested admin resource.
+  }
+});
+// [END verify_custom_claims]
