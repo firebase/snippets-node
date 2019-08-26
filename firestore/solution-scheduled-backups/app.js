b55b6ba472af8b74802fff477a1b83561e275da4
Naseem <naseemkullah@gmail.com>
2019-08-26 10:46:10 -0400

    Lint
    Signed-off-by: Naseem <naseemkullah@gmail.com>

--- firestore/solution-scheduled-backups/app.js
+++ firestore/solution-scheduled-backups/app.js
@@ -8,7 +8,7 @@ const app = express();
 // Trigger a backup
 app.get('/cloud-firestore-export', async (req, res) => {
   const auth = await google.auth.getClient({
-    scopes: ['https://www.googleapis.com/auth/datastore']
+    scopes: ['https://www.googleapis.com/auth/datastore'],
   });
 
   const accessTokenResponse = await auth.getAccessToken();
@@ -16,11 +16,11 @@ app.get('/cloud-firestore-export', async (req, res) => {
 
   const headers = {
     'Content-Type': 'application/json',
-    Authorization: 'Bearer ' + accessToken
+    Authorization: `Bearer ${accessToken}`,
   };
 
   const outputUriPrefix = req.param('outputUriPrefix');
-  if (!(outputUriPrefix && outputUriPrefix.indexOf('gs://') == 0)) {
+  if (!(outputUriPrefix && outputUriPrefix.indexOf('gs://') === 0)) {
     res.status(500).send(`Malformed outputUriPrefix: ${outputUriPrefix}`);
   }
 
@@ -30,11 +30,11 @@ app.get('/cloud-firestore-export', async (req, res) => {
   if (path.endsWith('/')) {
     path += timestamp;
   } else {
-    path += '/' + timestamp;
+    path += `/${timestamp}`;
   }
 
   const body = {
-    outputUriPrefix: path
+    outputUriPrefix: path,
   };
 
   // If specified, mark specific collections for backup
@@ -47,7 +47,7 @@ app.get('/cloud-firestore-export', async (req, res) => {
   const url = `https://firestore.googleapis.com/v1beta1/projects/${projectId}/databases/(default):exportDocuments`;
 
   try {
-    const response = await axios.post(url, body, { headers: headers });
+    const response = await axios.post(url, body, { headers });
     res
       .status(200)
       .send(response.data)
@@ -59,7 +59,7 @@ app.get('/cloud-firestore-export', async (req, res) => {
 
     res
       .status(500)
-      .send('Could not start backup: ' + e)
+      .send(`Could not start backup: ${e}`)
       .end();
   }
 });
