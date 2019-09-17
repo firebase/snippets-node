1f82a10cc55386c67e08a81f9eaa054bbb9d910a
JLara310 <1543140+jlara310@users.noreply.github.com>
2019-09-17 11:31:17 -0700

    Move constants outside of function

--- functions/firestore-export/index.js
+++ functions/firestore-export/index.js
@@ -1,32 +1,31 @@
 // [START fs_schedule_export]
 const functions = require('firebase-functions');
 const firestore = require('@google-cloud/firestore');
-
 const client = new firestore.v1.FirestoreAdminClient();
+
+// Replace BUCKET_NAME
+const bucket = 'gs://BUCKET_NAME';
 
 exports.scheduledFirestoreExport = functions.pubsub
                                             .schedule('every 24 hours')
                                             .onRun((context) => {
-   const databaseName = 
-       client.databasePath(process.env.GCP_PROJECT, '(default)');
+  const databaseName = 
+    client.databasePath(process.env.GCP_PROJECT, '(default)');
 
-   // Replace BUCKET_NAME
-   const bucket = 'gs://BUCKET_NAME';
-
-   return client.exportDocuments({
-       name: databaseName,
-       outputUriPrefix: bucket,
-       // Leave collectionIds empty to export all collections
-       // or set to a list of collection IDs to export,
-       // collectionIds: ['users', 'posts']
-       collectionIds: []
-       })
-   .then(responses => {
-       const response = responses[0];
-       console.log(`Operation Name: ${response['name']}`);
-   })
-   .catch(err => {
-       console.error(err);
-   });
+  return client.exportDocuments({
+    name: databaseName,
+    outputUriPrefix: bucket,
+    // Leave collectionIds empty to export all collections
+    // or set to a list of collection IDs to export,
+    // collectionIds: ['users', 'posts']
+    collectionIds: []
+    })
+  .then(responses => {
+    const response = responses[0];
+    console.log(`Operation Name: ${response['name']}`);
+  })
+  .catch(err => {
+    console.error(err);
+  });
 });
 // [END fs_schedule_export]
