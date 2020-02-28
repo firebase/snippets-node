90512afbc029ba2b81761f40d2171ff7b9710e05
Sam Stern <samstern@google.com>
2020-02-28 13:14:49 -0800

    Firebase app snippets
    Change-Id: I3ebd298744c38873dcf98a4df32423eeb0c72d2c

---
+++ firebaseapp/index.js
@@ -0,0 +1,21 @@
+const admin = require('firebase-admin');
+
+function multipleFirebaseApps() {
+    // [START firebase_options]
+    const secondaryServiceAccount = require('path/to/serviceAccountKey.json');
+
+    // All required options are specified by the service account,
+    // add service-specific configuration like databaseURL as needed.
+    const secondaryAppConfig = {
+        credential: admin.credential.cert(secondaryServiceAccount),
+        // databaseURL: 'https://<DATABASE_NAME>.firebaseio.com'
+    };
+    // [END firebase_options]
+
+    // [START firebase_secondary]
+    // Initialize another app with a different config
+    const secondary = admin.initializeApp(secondaryAppConfig, 'secondary');
+    // Access services, such as the Realtime Database
+    // const secondaryDatabase = secondary.database();
+    // [END firebase_secondary]
+}
