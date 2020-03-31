d7406c8082bc6d4942c4a8ece67426982fc8b182
Sam Stern <samstern@google.com>
2020-03-31 13:54:03 -0400

    Move to GitHub Actions (#88)

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,7 @@
 set -e
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 # 0) Bootstrap
 echo "Bootstrapping..."
 npm run lerna-bootstrap
@@ -9,4 +11,5 @@ echo "Linting..."
 npm run lint
 
 # 2) Run tests in emulator
-npx firebase --project="test-snippets-node" emulators:exec --only firestore "npm run lerna-test"
+source ${DIR}/set-credentials.sh
+npx firebase --project="firestore-snippets" emulators:exec --only firestore "npm run lerna-test"
