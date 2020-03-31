e645ddb31050d71a2b2c08f83bfc9e0e81f9936e
Sam <samstern@google.com>
2020-03-31 12:02:26 -0400

    Fix remaining issues

--- auth/get_service_account_tokens.js
+++ auth/get_service_account_tokens.js
@@ -17,6 +17,9 @@
 // [START get_service_account_tokens]
 const admin = require('firebase-admin');
 
+// [START_EXCLUDE silent]
+// @ts-ignore
+// [END_EXCLUDE]
 const serviceAccount = require('path/to/serviceAccountKey.json');
 const credential = admin.credential.cert(serviceAccount);
 
