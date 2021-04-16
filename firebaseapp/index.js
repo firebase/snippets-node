e95db7c7ab614facb8401987594e9ab85f7a8698
Sam <samstern@google.com>
2021-04-16 15:04:24 +0100

    FirebaseApp

--- firebaseapp/index.js
+++ firebaseapp/index.js
@@ -1,4 +1,4 @@
-const admin = require('firebase-admin');
+import { initializeApp, cert } from 'firebase-admin/app';
 
 function multipleFirebaseApps() {
     // [START firebase_options]
@@ -7,14 +7,14 @@ function multipleFirebaseApps() {
     // All required options are specified by the service account,
     // add service-specific configuration like databaseURL as needed.
     const secondaryAppConfig = {
-        credential: admin.credential.cert(secondaryServiceAccount),
+        credential: cert(secondaryServiceAccount),
         // databaseURL: 'https://<DATABASE_NAME>.firebaseio.com'
     };
     // [END firebase_options]
 
     // [START firebase_secondary]
     // Initialize another app with a different config
-    const secondary = admin.initializeApp(secondaryAppConfig, 'secondary');
+    const secondary = initializeApp(secondaryAppConfig, 'secondary');
     // Access services, such as the Realtime Database
     // const secondaryDatabase = secondary.database();
     // [END firebase_secondary]
