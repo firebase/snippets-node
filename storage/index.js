6a328cf884eb730b21936bd969ec255dfac429d8
Sam <samstern@google.com>
2021-05-11 15:53:38 +0100

    Lint fix

--- storage/index.js
+++ storage/index.js
@@ -2,13 +2,13 @@ const admin = require('firebase-admin');
 
 function initDefaultBucket() {
   // [START storage_init_default_bucket]
-  const admin = require("firebase-admin");
+  const admin = require('firebase-admin');
 
-  const serviceAccount = require("./path/to/serviceAccountKey.json");
+  const serviceAccount = require('./path/to/serviceAccountKey.json');
 
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    storageBucket: "<BUCKET_NAME>.appspot.com"
+    storageBucket: '<BUCKET_NAME>.appspot.com'
   });
 
   const bucket = admin.storage().bucket();
@@ -21,14 +21,14 @@ function initDefaultBucket() {
 
 function customBucket() {
   // [START storage_custom_bucket]
-  const bucket = admin.storage().bucket("my-custom-bucket");
+  const bucket = admin.storage().bucket('my-custom-bucket');
   // [END storage_custom_bucket]
 }
 
 function customApp() {
   const customApp = admin.initializeApp({
     // ...
-  }, "custom");
+  }, 'custom');
 
   // [START storage_custom_app]
   const bucket = customApp.storage().bucket();
