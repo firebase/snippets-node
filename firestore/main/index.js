0f434b51e8136c95ab54e99bddc626622bfb460c
Angel Caamal <acaamalcanul@google.com>
2026-01-06 18:23:00 +0000

    feat: use native recursiveDelete for collection cleanup

--- firestore/main/index.js
+++ firestore/main/index.js
@@ -949,35 +949,7 @@ async function multipleCursorConditions(db) {
 // [START firestore_data_delete_collection]
 async function deleteCollection(db, collectionPath, batchSize) {
   const collectionRef = db.collection(collectionPath);
-  const query = collectionRef.orderBy('__name__').limit(batchSize);
-
-  return new Promise((resolve, reject) => {
-    deleteQueryBatch(db, query, resolve).catch(reject);
-  });
-}
-
-async function deleteQueryBatch(db, query, resolve) {
-  const snapshot = await query.get();
-
-  const batchSize = snapshot.size;
-  if (batchSize === 0) {
-    // When there are no documents left, we are done
-    resolve();
-    return;
-  }
-
-  // Delete documents in a batch
-  const batch = db.batch();
-  snapshot.docs.forEach((doc) => {
-    batch.delete(doc.ref);
-  });
-  await batch.commit();
-
-  // Recurse on the next process tick, to avoid
-  // exploding the stack.
-  process.nextTick(() => {
-    deleteQueryBatch(db, query, resolve);
-  });
+  return await db.recursiveDelete(collectionRef);
 }
 
 // [END firestore_data_delete_collection]
